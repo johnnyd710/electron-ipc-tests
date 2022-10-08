a2555509f2c6b56d970c34d8b46cdd416c6a962e
johnnyd710 <19596966+johnnyd710@users.noreply.github.com>
2022-10-07 20:54:47 -0500

    init ipc main tests and ui

---
+++ README.md
@@ -0,0 +1,41 @@
+# electron-background-processing
+
+Update Oct 2021: Now using context isolation for renderer and background
+threads, see [Enable Context Isolation...](https://www.electronjs.org/docs/latest/tutorial/security#3-enable-context-isolation-for-remote-content)
+and [Context Isolation](https://www.electronjs.org/docs/latest/tutorial/context-isolation).
+
+Demo of background processing using Electron
+
+- Pool of background threads to match processors using hidden BrowserWindow instances
+- Using IPC to communicate between main, renderer and background threads, see [electron ipcMain](https://github.com/electron/electron/blob/master/docs/api/ipc-main.md)
+- Asynchronous message passing (synchronous is possible too but not used here)
+
+# Install and Run
+
+Install [NodeJS](https://nodejs.org/en/).
+
+```bash
+# Clone the repo
+git clone https://github.com/sgdan/electron-background-processing.git
+cd electron-background-processing
+
+# Installs Electron 1.8.8 (as specified in package.json)
+npm install
+
+# Runs "electron ." (main.js is specified in package.json)
+npm start
+```
+
+After starting up on the command line you should see the number of cpus e.g.
+
+```bash
+$ electron .
+
+cpus: 4
+```
+
+Use the **Send to main thread** button and you should see a message from the UI printed on the command line.
+
+The **Send to bg thread** button triggers a message from the UI to the main thread which passes it to one of the background threads. That background thread should respond back to the main thread which passes it to the UI. You should see the response immediately next to the button if a background thread was available.
+
+You can also **Do background work**. If you click it multiple times you should see your computer CPU rise to 100% usage. However, the UI should remain responsive. Further actions will be queued until they can be handled.
---
+++ main.js
@@ -0,0 +1,82 @@
+const { ipcMain, BrowserWindow, app } = require("electron");
+const path = require("path");
+app.allowRendererProcessReuse = true;
+const cpus = require("os").cpus().length;
+console.log("cpus: " + cpus);
+
+// stack of available background threads
+var available = [];
+
+// queue of tasks to be done
+var tasks = [];
+
+// hand the tasks out to waiting threads
+function doIt() {
+  while (available.length > 0 && tasks.length > 0) {
+    var task = tasks.shift();
+    available.shift().send(task[0], task[1]);
+  }
+  renderer.webContents.send("status", available.length, tasks.length);
+}
+
+// Create a hidden background window
+function createBgWindow() {
+  result = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  result.loadURL("file://" + __dirname + "/background.html");
+  result.on("closed", () => {
+    console.log("background window closed");
+  });
+  return result;
+}
+
+app.whenReady().then(function () {
+  // Create the "renderer" window which contains the visible UI
+  renderer = new BrowserWindow({
+    width: 500,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  renderer.loadURL("file://" + __dirname + "/renderer.html");
+  renderer.show();
+  renderer.on("closed", () => {
+    // call quit to exit, otherwise the background windows will keep the app running
+    app.quit();
+  });
+
+  // create background thread for each cpu
+  createBgWindow();
+
+  // Main thread can receive directly from windows
+  ipcMain.on("to-main", (event, arg) => {
+    console.log(arg);
+    renderer.webContents.send("to-renderer")
+  });
+
+  // Windows can talk to each other via main
+  ipcMain.on("for-renderer", (event, arg) => {
+    renderer.webContents.send("to-renderer", arg);
+  });
+  ipcMain.on("for-background", (event, arg) => {
+    tasks.push(["message", arg]);
+    doIt();
+  });
+
+  // heavy processing done in the background thread
+  // so UI and main threads remain responsive
+  ipcMain.on("assign-task", (event, arg) => {
+    tasks.push(["task", arg]);
+    doIt();
+  });
+
+  ipcMain.on("ready", (event, arg) => {
+    available.push(event.sender);
+    doIt();
+  });
+});
